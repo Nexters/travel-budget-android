b1cc12fa747e3d4805bb435a44ca9b74511bea1f
sodp5 <sodp5@naver.com>
2020-07-27 17:32:10 +0900

    의미없는 로그 삭제

--- app/src/main/java/com/nexters/travelbudget/utils/widget/piechart/PieChartView.kt
+++ app/src/main/java/com/nexters/travelbudget/utils/widget/piechart/PieChartView.kt
@@ -5,7 +5,6 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import kotlin.collections.ArrayList
@@ -51,8 +50,6 @@ class PieChartView @JvmOverloads constructor(
         }
 
         var anglePointer = 0f
-
-        Log.d("dataListSize", "${dataList.size}")
 
         anglePointList.clear()
 
