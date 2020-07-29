28b4f147d03bc6d7586176a3c87e271c52ab42b9
sodp5 <sodp5@naver.com>
2020-07-29 16:17:57 +0900

    SelectDateBottomSheetDialog adapter연결

--- app/src/main/java/com/nexters/travelbudget/ui/select_date/SelectDateBottomSheetDialog.kt
+++ app/src/main/java/com/nexters/travelbudget/ui/select_date/SelectDateBottomSheetDialog.kt
@@ -1,8 +1,12 @@
 package com.nexters.travelbudget.ui.select_date
 
+import android.os.Bundle
+import androidx.lifecycle.Observer
 import com.nexters.travelbudget.R
 import com.nexters.travelbudget.databinding.BottomSheetSelectDateBinding
 import com.nexters.travelbudget.ui.base.BaseBottomSheetDialogFragment
+import com.nexters.travelbudget.ui.select_date.adapter.SelectDateRVAdapter
+import com.nexters.travelbudget.utils.DLog
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SelectDateBottomSheetDialog
@@ -10,4 +14,26 @@ class SelectDateBottomSheetDialog
     R.layout.bottom_sheet_select_date
 ) {
     override val viewModel: SelectDateViewModel by viewModel()
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.rvDateList.adapter = SelectDateRVAdapter {
+            DLog.d(it)
+        }.apply {
+            add("2018.7.28")
+            add("2019.7.28")
+            add("2020.7.28")
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        with(viewModel) {
+            dismissEvent.observe(this@SelectDateBottomSheetDialog, Observer {
+                this@SelectDateBottomSheetDialog.dismiss()
+            })
+        }
+    }
 }
--- app/src/main/java/com/nexters/travelbudget/ui/select_date/SelectDateViewModel.kt
+++ app/src/main/java/com/nexters/travelbudget/ui/select_date/SelectDateViewModel.kt
@@ -1,6 +1,12 @@
 package com.nexters.travelbudget.ui.select_date
 
 import com.nexters.travelbudget.ui.base.BaseViewModel
+import com.nexters.travelbudget.utils.lifecycle.SingleLiveEvent
 
 class SelectDateViewModel : BaseViewModel() {
+    val dismissEvent = SingleLiveEvent<Unit>()
+
+    fun dismiss() {
+        dismissEvent.call()
+    }
 }
