2f036079aa543d1d73c3f44769bc000a3e3ef8e8
YKW93 <rhkddn1657@gmail.com>
2020-07-31 17:38:13 +0900

    로그아웃 로직 추가 및 rx 통신 핸들링하는 클래스 추가

--- app/src/main/java/com/nexters/travelbudget/ui/base/TravelApplication.kt
+++ app/src/main/java/com/nexters/travelbudget/ui/base/TravelApplication.kt
@@ -2,11 +2,16 @@ package com.nexters.travelbudget.ui.base
 
 import android.app.Application
 import android.content.Context
+import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
 import com.kakao.auth.KakaoSDK
+import com.kakao.usermgmt.UserManagement
+import com.kakao.usermgmt.callback.LogoutResponseCallback
 import com.nexters.travelbudget.BuildConfig
+import com.nexters.travelbudget.data.local.prefs.UserTokenManager
 import com.nexters.travelbudget.di.*
+import com.nexters.travelbudget.ui.login.LoginActivity
 import com.nexters.travelbudget.ui.login.kakao.KakaoSDKAdapter
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -40,6 +45,18 @@ class TravelApplication : Application() {
         }
 
         return debuggable
+    }
+
+    fun logout() {
+        UserManagement.getInstance().requestLogout(object : LogoutResponseCallback() {
+            override fun onCompleteLogout() {
+                UserTokenManager.deleteUserToken(this@TravelApplication)
+                val intent = LoginActivity.getIntent(this@TravelApplication).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+                startActivity(intent)
+            }
+        })
     }
 
     companion object {
---
+++ app/src/main/java/com/nexters/travelbudget/utils/observer/TripieSingleObserver.kt
@@ -0,0 +1,35 @@
+package com.nexters.travelbudget.utils.observer
+
+import android.widget.Toast
+import com.nexters.travelbudget.R
+import com.nexters.travelbudget.ui.base.TravelApplication
+import com.nexters.travelbudget.utils.ext.isNetworkConnected
+import io.reactivex.observers.DisposableSingleObserver
+import retrofit2.HttpException
+
+/**
+ * 서버 통신 시 발생하는 에러 핸들링하기 위한 클래스
+ *
+ * @author Wayne
+ * @since v1.0.0 / 2020.07.31
+ */
+
+abstract class TripDisposableSingleObserver<T> :
+    DisposableSingleObserver<T>() {
+
+    override fun onError(e: Throwable) {
+        with(TravelApplication.instance) {
+            if ((e as? HttpException)?.code() == 401) {
+                logout()
+            } else {
+                if (!isNetworkConnected()) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.network_disconnected_notice),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+}
