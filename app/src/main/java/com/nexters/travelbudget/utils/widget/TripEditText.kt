c7984595502288d75289fd78ef3899f62fabe27d
sodp5 <sodp5@naver.com>
2020-08-04 15:04:35 +0900

    TripEditText bottom padding값 추가

--- app/src/main/java/com/nexters/travelbudget/utils/widget/TripEditText.kt
+++ app/src/main/java/com/nexters/travelbudget/utils/widget/TripEditText.kt
@@ -2,16 +2,26 @@ package com.nexters.travelbudget.utils.widget
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.DisplayMetrics
+import android.util.TypedValue
 import android.widget.EditText
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.updatePadding
 import com.nexters.travelbudget.R
 
 class TripEditText @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = R.attr.editTextStyle
+    defStyleAttr: Int = 0
 ) : AppCompatEditText(context, attrs, defStyleAttr) {
     init {
+        updatePadding(
+            bottom = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                12f,
+                resources.displayMetrics
+            ).toInt()
+        )
         setBackgroundResource(R.drawable.bg_edit_text_underground_default)
         setHintTextColor(resources.getColor(R.color.colorTextHint, null))
         setTextColor(resources.getColor(R.color.colorTextBlack, null))
