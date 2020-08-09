38c7995641812d5d0eeee1b2859d368488dc3ea8
YKW93 <rhkddn1657@gmail.com>
2020-08-09 15:15:55 +0900

    다녀온 여행화면에 adapter 적용

--- app/src/main/java/com/nexters/travelbudget/ui/main/record/RecordedTravelFragment.kt
+++ app/src/main/java/com/nexters/travelbudget/ui/main/record/RecordedTravelFragment.kt
@@ -1,12 +1,16 @@
 package com.nexters.travelbudget.ui.main.record
 
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.nexters.travelbudget.R
 import com.nexters.travelbudget.databinding.FragmentRecordedTravelBinding
 import com.nexters.travelbudget.ui.base.BaseFragment
 import com.nexters.travelbudget.ui.main.MainViewModel
+import com.nexters.travelbudget.utils.CustomItemDecoration
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -27,6 +31,7 @@ class RecordedTravelFragment :
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setSwipeRefreshLayout()
+        setRecordedTravelRV()
     }
 
     override fun onResume() {
@@ -41,6 +46,35 @@ class RecordedTravelFragment :
         binding.swipeRefreshLayout.run {
             setOnRefreshListener(this@RecordedTravelFragment)
             setColorSchemeColors(resources.getColor(R.color.fill_grey_1, activity?.theme))
+        }
+    }
+
+    private fun setRecordedTravelRV() {
+        binding.rvRecordedTravel.run {
+            adapter = RecordingTravelRVAdapter { tripRecordResponse ->
+                // TODO 여행 상세 화면으로 연결 작업
+                Toast.makeText(context, "상세 화면 전환", Toast.LENGTH_LONG).show()
+            }
+            addItemDecoration(object : CustomItemDecoration() {
+                override fun setSpacingForDirection(
+                    outRect: Rect,
+                    layoutManager: RecyclerView.LayoutManager?,
+                    position: Int,
+                    itemCount: Int
+                ) {
+                    if (position == 0) {
+                        outRect.top =
+                            context.resources.getDimensionPixelSize(R.dimen.spacing_size_30dp)
+                    }
+                    outRect.bottom =
+                        context.resources.getDimensionPixelSize(R.dimen.spacing_size_20dp)
+                    outRect.left =
+                        context.resources.getDimensionPixelSize(R.dimen.spacing_size_24dp)
+                    outRect.right =
+                        context.resources.getDimensionPixelSize(R.dimen.spacing_size_24dp)
+                }
+
+            })
         }
     }
 
