8981b897fa24d0c80c7380bfd4e558746dd894e1
sodp5 <sodp5@naver.com>
2020-08-21 03:00:07 +0900

    enum값 가져오는 함수 추가

--- app/src/main/java/com/nexters/travelbudget/model/enums/SpendCategoryEnum.kt
+++ app/src/main/java/com/nexters/travelbudget/model/enums/SpendCategoryEnum.kt
@@ -15,19 +15,19 @@ enum class SpendCategoryEnum(
     @DrawableRes val defaultRes: Int,
     @DrawableRes val selectedRes: Int
 ) {
-    MOVE(
+    TRAFFIC(
         "교통",
         "TRAFFIC",
         R.drawable.ic_category_transportation_disable_32dp,
         R.drawable.ic_category_transportation_32dp
     ),
-    STAY(
+    SLEEP(
         "숙박",
         "SLEEP",
         R.drawable.ic_category_hotel_disable_32dp,
         R.drawable.ic_category_hotel_32dp
     ),
-    MEAL(
+    FOOD(
         "식비",
         "FOOD",
         R.drawable.ic_category_meal_disable_32dp,
@@ -51,10 +51,40 @@ enum class SpendCategoryEnum(
         R.drawable.ic_category_culture_disable_32dp,
         R.drawable.ic_category_culture_32dp
     ),
-    OTHERS(
+    ETC(
         "기타",
         "ETC",
         R.drawable.ic_category_etc_disable_32dp,
         R.drawable.ic_category_etc_32dp
     );
+
+    companion object {
+        fun getEnumByTitleKor(kor: String): SpendCategoryEnum {
+            return when (kor) {
+                "식비" -> FOOD
+                "문화" -> CULTURE
+                "교통" -> TRAFFIC
+                "쇼핑" -> SHOPPING
+                "숙박" -> SLEEP
+                "간식" -> SNACK
+                "기타" -> ETC
+
+                else -> ETC
+            }
+        }
+
+        fun getEnumByTitleEng(eng: String): SpendCategoryEnum {
+            return when (eng) {
+                "FOOD" -> FOOD
+                "CULTURE" -> CULTURE
+                "TRAFFIC" -> TRAFFIC
+                "SHOPPING" -> SHOPPING
+                "SLEEP" -> SLEEP
+                "SNACK" -> SNACK
+                "ETC" -> ETC
+
+                else -> ETC
+            }
+        }
+    }
 }
